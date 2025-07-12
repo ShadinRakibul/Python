0b0c7d9b306c8b7d3a32d11a5d1054c2650d3bd0
ShadinRakibul <21221203017@cse.bubt.edu.bd>
2025-07-12 22:12:40 +0600

    first commit

--- Chapter 1/first.py
+++ Chapter 1/first.py
@@ -1,1 +1,6 @@
 print("Hello, World!")
+a =input("Enter a number: ")
+b=input("Enter another number: ")
+sum = int(a) + int(b)   
+print("The sum is:", sum)
+print("This is the first Python script in Chapter 1.")
