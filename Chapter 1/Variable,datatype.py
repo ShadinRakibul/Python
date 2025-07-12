36eb386396579b10466d50a937dbbb10f653f233
ShadinRakibul <21221203017@cse.bubt.edu.bd>
2025-07-12 22:47:46 +0600

    Second

---
+++ Chapter 1/Variable,datatype.py
@@ -0,0 +1,13 @@
+a=10
+b=True
+c=3.14  
+d="Hello"
+print("Variable a:", a)
+print("Variable b:", b)
+print("Variable c:", c)
+print("Variable d:", d)
+print("Type of a is", type(a))
+print("Type of b is", type(b))
+print("Type of c is", type(c))  
+print("Type of d is", type(d))
+print("This is the Variable and datatype script in Chapter 1.")
