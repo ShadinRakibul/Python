0ae18e26d3a4d023219e7bc24ea3b11baae53fa0
ShadinRakibul <21221203017@cse.bubt.edu.bd>
2025-07-12 22:58:44 +0600

    third commit

---
+++ Chapter 1/list,tuple.py
@@ -0,0 +1,7 @@
+list=['apple', 'banana', 'cherry',[-8.20],3.14,True]
+print("List:", list)
+tuple=('red', 'green', 'blue', 42, 3.14, False)
+print("Tuple:", tuple)
+print("Type of list is", type(list))
+print("Type of tuple is", type(tuple))
+print("This is the List and Tuple script in Chapter 1.")
