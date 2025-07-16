95ce94a06e372484619727357c69ee6a084fe4ee
ShadinRakibul <21221203017@cse.bubt.edu.bd>
2025-07-16 22:31:37 +0600

    string-slicing

---
+++ Chapter 2/String_Slicing.py
@@ -0,0 +1,11 @@
+fruit="Mango, Banana, Cherry, Apple, Orange"
+len1=len(fruit)
+print("Length of the string:", len1)
+print(fruit[0])  # first character  
+print (fruit[0:6])  # slicing from index 1 to 5
+print(fruit[1:])  # slicing from index 1 to end
+print(fruit[:6])  # slicing from start to index 5
+print(fruit[-1])  # last character
+print(fruit[-2])  # second last character
+print(fruit[-3:])  # last three characters
+print(fruit[-3:-1])  # slicing with negative indices
