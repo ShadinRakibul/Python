7d2501850a2753121f74070babfe7e58b9b8d0bf
ShadinRakibul <21221203017@cse.bubt.edu.bd>
2025-07-17 23:18:09 +0600

    nested if condition

---
+++ Chapter 2/nested.py
@@ -0,0 +1,13 @@
+num=int(input("Enter a number: "))
+if num < 0:
+    print("Negative number")
+elif num > 0:
+    if num <=10:
+        print("Number is positive and in between 10)")
+    else:
+        print("Number is greater than 10")
+elif num == 0:
+    print("Zero")
+else:
+    print("Number is greater than 10")
+# Example of using nested if statements
