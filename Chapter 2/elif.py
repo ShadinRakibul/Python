edc50853b919c9b46e51d098c0feea780bd01e96
ShadinRakibul <21221203017@cse.bubt.edu.bd>
2025-07-17 22:37:29 +0600

    new example added

---
+++ Chapter 2/elif.py
@@ -0,0 +1,6 @@
+apple_price = 180
+budget = int(input("Enter your budget: "))
+if budget >= apple_price:
+    print("You can buy the apple.")
+else:
+    print("You cannot buy the apple.")
--- Chapter 2/if-else_conditional_statement.py
+++ Chapter 2/if-else_conditional_statement.py
@@ -31,3 +31,11 @@ if a>18:
 else:
     print("You are not eligible to vote.")
     
+# new example added
+apple_price=180
+budget = int(input("Enter your budget: "))
+if budget >= apple_price:
+    print("You can buy the apple.")
+else:
+    print("You cannot buy the apple.")
+    
